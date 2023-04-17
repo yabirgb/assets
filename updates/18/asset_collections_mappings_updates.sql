cd0588384648030928afcbf86697e32343a710c9
Yabir Benchakhtir <yabirg@protonmail.com>
2023-04-17 12:05:10 +0200

    Update for 1.28

---
+++ updates/18/asset_collections_mappings_updates.sql
@@ -0,0 +1,26 @@
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (216, "eip155:42161/erc20:0x912CE59144191C1204E64559FE8253a0e49E6548");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (216, "eip155:1/erc20:0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (128, "eip155:10/erc20:0xc40F949F8a4e094D1b49a23ea9241D289B7b2819");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (128, "eip155:42161/erc20:0x93b346b6BC2548dA6A1E7d98E9a421B42541425b");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (217, "eip155:10/erc20:0x9Bcef72be871e61ED4fBbc7630889beE758eb81D");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (217, "eip155:42161/erc20:0xEC70Dcb4A1EFa46b8F2D97C310C9c4790ba5ffA8");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (217, "eip155:1/erc20:0x9559Aaa82d9649C7A7b220E7c461d2E74c9a3593");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:1/erc20:0xac3E018457B222d93114458476f3E3416Abbe38F");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:10/erc20:0x484c2D6e3cDd945a8B2DF735e079178C1036578c");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:42161/erc20:0x95aB45875cFFdba1E5f451B950bC2E42c0053f39");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:137/erc20:0x6d1FdBB266fCc09A16a22016369210A15bb95761");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:56/erc20:0x3Cd55356433C89E50DC51aB07EE0fa0A95623D53");
+*
+INSERT INTO multiasset_mappings(collection_id, asset) VALUES (218, "eip155:250/erc20:0xb90CCD563918fF900928dc529aA01046795ccb4A");
+*
---
+++ updates/18/asset_collections_updates.sql
@@ -0,0 +1,6 @@
+INSERT INTO asset_collections(id, name, symbol) VALUES (216, "Arbitrum", "ARB");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (217, "Rocket Pool ETH", "rETH");
+*
+INSERT INTO asset_collections(id, name, symbol) VALUES (218, "Staked Frax Ether", "sfrxETH");
+*
